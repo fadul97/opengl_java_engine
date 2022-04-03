445a5b751dbbc14887d3042f6aa51e6d8292f62b
Leonardo Fadul <lsffadul@gmail.com>
2022-04-03 08:57:10 -0300

    Hello, Window

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,39 @@
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        if(!glfwInit())
+            System.out.println("Failed to initialize GLFW.");
+
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        // Create glfwWindow object
+        long glfwWindow = glfwCreateWindow(800, 600, "LearnOpenGL Java Engine", NULL, NULL);
+
+        if(glfwWindow == NULL){
+            System.out.println("Failed to create glfwWindow.");
+            glfwTerminate();
+        }
+
+        glfwMakeContextCurrent(glfwWindow);
+        glfwShowWindow(glfwWindow);
+
+        // Init glad(?)
+        GL.createCapabilities();
+
+
+        while (!glfwWindowShouldClose(glfwWindow)){
+            glfwSwapBuffers(glfwWindow);
+            glfwPollEvents();
+        }
+
+        glfwTerminate();
     }
 }
